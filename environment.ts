ccac70da4261b763bfb407b35cc4f3729ed682a8
Akshay Mistry <akshay.mistry93@gmail.com>
2025-01-03 18:05:01 +0530

    Added responsive css

--- environment.ts
+++ environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-    apiKey: "AIzaSyCN8-Din_zEKfHYlmeEl9h-21Fr5N7HX14",
-    authDomain: "uxay2025.firebaseapp.com",
-    projectId: "uxay2025",
-    storageBucket: "uxay2025.firebasestorage.app",
-    messagingSenderId: "291843150498",
-    appId: "1:291843150498:web:ff6fcd650583d9a39bfdfb",
-    measurementId: "G-W2WJNG1QZ5"
-}
+        apiKey: "AIzaSyDebyMWM3vFvobsFuSxTpWzMHoODO808pk",
+        authDomain: "uxaydesigns-f4e77.firebaseapp.com",
+        projectId: "uxaydesigns-f4e77",
+        storageBucket: "uxaydesigns-f4e77.firebasestorage.app",
+        messagingSenderId: "145359753371",
+        appId: "1:145359753371:web:293bd621744c506c91763e",
+        measurementId: "G-297XSJHQH8"
+    }
 };
